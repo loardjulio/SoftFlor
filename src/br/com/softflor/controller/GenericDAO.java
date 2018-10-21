518a281d857da0991e7e44c4a6e2378711b5619f
Julio <Julio@192.168.56.1>
2018-10-21 12:26:33 -0300

    Ajustes no código

--- src/br/com/softflor/controller/GenericDAO.java
+++ src/br/com/softflor/controller/GenericDAO.java
@@ -9,74 +9,57 @@ import br.com.softflor.conexao.ConectaBD;
 import br.com.softflor.entidades.EntidadeBase;
 import java.io.Serializable;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author Julio
+ * @param <T>
  */
 public class GenericDAO<T extends EntidadeBase> extends ConectaBD {
 
     EntityManager em = getEntityManager();
 
     public T salvarOuAtualizar(T t) {
-
         try {
             em.getTransaction().begin();
             if (t.getId() == null) { //verifica se é nulo ou não
-                em.persist(t); // executa o insert
+                em.persist(t); // se for nulo executa o insert
                 JOptionPane.showMessageDialog(null, "Salvo!");
-            } else { //se nao for nulo ele salva
-                if (!em.contains(t)) {
-                    if (em.find(t.getClass(), t.getId()) == null) {
-                        throw new Exception("Erro ao atualizar!");
-                    }
-                }
+            } else { //se nao for nulo 
                 t = em.merge(t); //executa o update
                 JOptionPane.showMessageDialog(null, "Atualizado!");
             }
             em.getTransaction().commit();
-            
         } catch (Exception e) {
-            System.out.println(e);
             JOptionPane.showMessageDialog(null, "Erro ao Salvar/Atualizar");
             FechaConexao();
         }
-
         return t;
-
     }
-    
-    
 
     public void remover(Class<T> clazz, Serializable id) { //clazz é so pra diferenciar da palavra reservada
-        T t = em.find(clazz, id); 
-        
-        try {             
+        T t = em.find(clazz, id); //busca  o objeto         
+        try {
             em.getTransaction().begin();
             em.remove(t); //executa o delete
             em.getTransaction().commit();
             JOptionPane.showMessageDialog(null, "Excluido com sucesso.");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "(GenericDao) Erro ao remover o item: "+e);            
-             FechaConexao();
-             throw e;
-        } 
+            JOptionPane.showMessageDialog(null, "(GenericDao) Erro ao remover o item: " + e);
+            FechaConexao();
+            throw e;
+        }
     }
 
     public T buscarPorId(Class<T> clazz, Serializable id) {
-
         T t = null;
-
-        try {           
-            System.out.println("Tentei buscar " + id);
-            t = em.find(clazz, id);
+        try {
+            t = em.find(clazz, id); //Busca o objeto
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao buscar por ID");
             FechaConexao();
-        } 
+        }
         return t;
     }
 }
